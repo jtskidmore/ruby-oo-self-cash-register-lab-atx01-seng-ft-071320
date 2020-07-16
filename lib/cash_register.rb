ac89b53a1e10ae591487e696ecc51bdc7aa56ad5
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-16 21:21:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,9 @@ class CashRegister
     @quantity = quantity
     @price = price*quantity.to_f
     @total += @price
-    @items << self
+    quantity.times do 
+      self.class.items << @title
+    end
     @last_transaction = self
   end
 
@@ -32,7 +34,7 @@ class CashRegister
   end
 
   def items
-    @items
+    @@items
   end
 
 end
